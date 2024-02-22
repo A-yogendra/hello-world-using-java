2230dc347457944c0b36b3d0923eeb33270f6414
A-yogendra <147177781+A-yogendra@users.noreply.github.com>
2024-02-22 11:34:25 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+//This program is used to print hello
+import java.io.*;
+class Demo1
+{
+public static void main(String array[])
+{System.out.println("Hello"+"\t"+"BYE");
+System.out.println("hello");
+System.out.printf("hello");
+}//main
+}//Demo1
